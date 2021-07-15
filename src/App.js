bd1ba60fec24245e0c108720b6c0e633a046c572
Greg Custer <gregory.custer@gmail.com>
2021-07-14 21:48:55 -0400

    Added basic canvas and game feed to app. Testing out layout.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Open TableTop RPG
-        </p>
-      </header>
+      <AppCanvas/>
+      <GameFeed/>
     </div>
   );
+}
+
+function AppCanvas() {
+  return (
+    <canvas className="App-canvas"/>
+  );
+}
+
+function GameFeed() {
+  return (
+    <div className="App-game-feed">
+        <p>Open TableTop RPG</p>
+        <ul/>
+        <form id="feed-form" action="">
+          <input id="feed-input" autocomplete="off" /><button>Send</button>
+        </form>
+    </div>
+  )
 }
 
 export default App;
