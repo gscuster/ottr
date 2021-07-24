d5c46fc9ff4c0b08baf38a61e09d25782201bf30
Greg <gregory.custer@gmail.com>
2021-07-24 06:56:48 -0400

    Added support for server commands to Message

--- server/Message.js
+++ server/Message.js
@@ -1,7 +1,24 @@
 export const onMessage = (msg, io, socket) => {
-  console.log('message: ' + msg);
-    io.emit('message', {
-      username: socket.username,
-      message: msg
-    });
+  console.log('Received: ' + msg);
+  switch (parseMessageType(msg)) {
+    case '/roll':
+    case '/r':
+      console.log('Rolling');
+      break;
+    default:
+      io.emit('message', {
+        username: socket.username,
+        message: msg
+      });
+  }
+}
+
+const parseMessageType = (msg) => {
+  const spacePattern = /\s+/;
+  const msgFormatted = msg.trim().split(spacePattern)
+  if (msgFormatted.length > 0 &&  msgFormatted[0].startsWith('/')) {
+    return msgFormatted[0];
+  }
+
+  return 'message';
 }
