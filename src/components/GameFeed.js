3a8f9011b83e1c566160a651173f10e74ea84689
Greg <gregory.custer@gmail.com>
2021-08-08 16:21:45 -0400

    Auto-scroll to bottom when items are added to feed

--- src/components/GameFeed.js
+++ src/components/GameFeed.js
@@ -3,9 +3,21 @@ import {FeedItem} from './FeedItem.js';
 import {FeedForm} from './FeedForm.js';
 import {UsernameForm} from './UsernameForm.js';
 import { UsernameText } from './UsernameText';
+import React, { useEffect, useRef } from "react";
 
 export const GameFeed = ({feed=[], sendMessage, userSelected, selectUserName, 
   username, editUserActive, setEditUserActive, editUserName}) => { 
+
+  const feedEndRef = useRef(null);
+  const scrollToBottom = () => {
+    feedEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(scrollToBottom, [feed]);
+
+  const onScrollEvent = (e) => {
+    console.log(e);
+  }
+  
   return (
     <div className="game-feed">
       <div className="header">
@@ -16,11 +28,13 @@ export const GameFeed = ({feed=[], sendMessage, userSelected, selectUserName,
             setEditUserActive={setEditUserActive}
             editUserName={editUserName}/>}
       </div>
-    
-      <ul className="feed-list">
-        {feed.map((feedItem, i) => 
-          <FeedItem key={i} feedItem={feedItem} prev={feed[i-1]} next={feed[i+1]}/>)}
-      </ul>
+      <div className="body" onScroll={onScrollEvent}>
+        <ul className="feed-list">
+          {feed.map((feedItem, i) => 
+            <FeedItem key={i} feedItem={feedItem} prev={feed[i-1]} next={feed[i+1]}/>)}
+        </ul>
+        <div ref={feedEndRef}></div>
+      </div>
     
       <div className="footer">
         {userSelected ? 
