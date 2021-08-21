a66a151e9fd540b81fe030d85990c4ce13636ba2
Greg <gregory.custer@gmail.com>
2021-08-21 06:08:01 -0400

    Switched cors origin to accept all.

--- server/server.js
+++ server/server.js
@@ -71,10 +71,7 @@ io.on('connection', socket => {
 
 io.listen(port, {
   cors: {
-    origin: ["http://localhost:3000", 
-      "http://192.168.0.191:3000", 
-      "http://192.168.0.4:3000",
-      "http://192.168.1.91:3000"]
+    origin: '*'
   }
 });
 
